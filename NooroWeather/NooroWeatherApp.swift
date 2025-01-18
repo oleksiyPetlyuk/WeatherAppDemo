90f6bde2f775671e8a642c18cb5cb363d5fb3aa2
Oleksiy Petlyuk <oleksiy.petlyuk94@gmail.com>
2025-01-18 14:10:53 -0600

    Initial Commit

---
+++ NooroWeather/NooroWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroWeatherApp.swift
+//  NooroWeather
+//
+//  Created by Petlyuk, Oleksiy on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct NooroWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
